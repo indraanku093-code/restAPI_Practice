24cc27ddd49717cd86db0fe8684b91ece2caab17
indrajeet <indrajeet093@gmail.com>
2021-01-31 11:09:00 +0530

    add comment

--- src/main/java/restAPI_Utilities/EndPoints.java
+++ src/main/java/restAPI_Utilities/EndPoints.java
@@ -1,6 +1,8 @@
 package restAPI_Utilities;
 
 public interface EndPoints {
+//End points are there
+
 	String NORMALGET = "/api/users?page=2";
 	String GETID = "/api/users/{id}";
 	String POSTURI = "/api/users";
